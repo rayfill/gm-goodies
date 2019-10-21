539a64865e9f2197859f40d6028f349cd8b9f0ec
alfeim <rayfill@gmail.com>
2019-10-21 23:26:34 +0900

    a

--- proxy.js
+++ proxy.js
@@ -3,58 +3,56 @@
 let proxy = null;
 
 
-(() => {
-  function genTrap(options) {
-    return (name) => {
-      if (name in options) {
-	return options[name];
-      }
-      return null;
+function genTrap(options) {
+  return (name) => {
+    if (name in options) {
+      return options[name];
     }
-  };
+    return null;
+  }
+};
   
-  const instanceHandler = (gettrap, settrap) => {
-    gettrap = gettrap != null ? gettrap : () => { return null };
-    settrap = settrap != null ? settrap : () => { return null };
+const instanceHandler = (gettrap, settrap) => {
+  gettrap = gettrap != null ? gettrap : () => { return null };
+  settrap = settrap != null ? settrap : () => { return null };
 
-    return {
-      get: (target, prop, receiver) => {
+  return {
+    get: (target, prop, receiver) => {
 
-	let trapFunc = gettrap(prop);
-	if (trapFunc !== null) {
-	  return trapFunc(receiver, target, prop, undefined);
-	}
-	
-	let val = target[prop];
-	if (typeof val === "function") {
-	  return (...args) => {
-	    return val.apply(target, args);
-	  }
-	}
-	return val;
-      },
-
-      set: (target, prop, value, receiver) => {
-
-	let trapFunc = settrap(prop);
-	if (trapFunc !== null) {
-	  return trapFunc(receiver, target, prop, value);
-	}
-	return target[prop] = value;
+      let trapFunc = gettrap(prop);
+      if (trapFunc !== null) {
+	return trapFunc(receiver, target, prop, undefined);
       }
-    }
-  };
-
-  const constructHandler = (gettrap, settrap) => {
-    return {
-      construct: (target, args) => {
-	let instance = new (target.bind.apply(target, args));
-	return new Proxy(instance, instanceHandler(gettrap, settrap));
+      
+      let val = target[prop];
+      if (typeof val === "function") {
+	return (...args) => {
+	  return val.apply(target, args);
+	}
       }
-    }
-  };
+      return val;
+    },
 
-  proxy = (clazz, gettrap, settrap) => {
-    return new Proxy(clazz, constructHandler(gettrap, settrap));
-  };
-})();
+    set: (target, prop, value, receiver) => {
+
+      let trapFunc = settrap(prop);
+      if (trapFunc !== null) {
+	return trapFunc(receiver, target, prop, value);
+      }
+      return Reflect.set(target, prop, value);
+    }
+  }
+};
+
+const constructHandler = (gettrap, settrap) => {
+  return {
+    construct: (target, args) => {
+      let instance = new (target.bind.apply(target, args));
+      return new Proxy(instance, instanceHandler(gettrap, settrap));
+    }
+  }
+};
+
+proxy = (clazz, gettrap, settrap) => {
+  return new Proxy(clazz, constructHandler(gettrap, settrap));
+};
