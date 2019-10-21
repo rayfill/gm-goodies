6c077300881257a19266495708de3abada2f5bb5
alfeim <rayfill@gmail.com>
2019-10-21 22:38:41 +0900

    fix proxy design

--- proxy.js
+++ proxy.js
@@ -1,53 +1,61 @@
 'use strict';
 
-let TrapClass = null;
+let proxy = null;
+
+
 (() => {
-    const instanceHandler = (traps, trapCheck) => {
+  function genTrap(options) {
+    return (name) => {
+      if (name in options) {
+	return options[name];
+      }
+      return null;
+    }
+  };
+  
+  const instanceHandler = (gettrap, settrap) => {
+    gettrap = gettrap != null ? gettrap : () => { return null };
+    settrap = settrap != null ? settrap : () => { return null };
 
-	return {
-            get: (target, prop, receiver) => {
-		let value = target[prop];
-                let offset = trapCheck(prop);
-		
-		if (typeof value === "function") {
-		    const func = value;
-		    
-                    if (offset >= 0) {
-			return (...args) => {
-                            return traps(offset)(proxy, target, func, args);
-			}
-                    }
-		    
-                    return (...args) => {
-			return func.apply(target, args);
-                    }
-		} else if (offset >= 0) {
-		    return traps(offset)(proxy, value);
-		}
-		return value;
-            },
+    return {
+      get: (target, prop, receiver) => {
 
-            set: (target, prop, value) => {
-		let offset = trapCheck(prop);
-		if (offset >= 0) {
-		    traps(offset)(proxy, target, prop, value);
-		} else {
-		    target[prop] = value;
-		}
-            }
+	let trapFunc = gettrap(prop);
+	if (trapFunc != null) {
+	  return trapFunc(receiver, target, prop, undefined);
 	}
-    };
-
-    const constructHandler = (traps, trapCheck) => {
-	return {
-            construct: (target, args) => {
-		let instance = new (target.bind.apply(target, args));
-		return new Proxy(instance, instanceHandler(traps, trapCheck));
-            }
+	
+	let val = target[prop];
+	if (typeof val === "function") {
+	  return (...args) => {
+	    return val.apply(target, args);
+	  }
 	}
-    };
+	return val;
+      },
 
-    TrapClass = (clazz, traps, trapCheck) => {
-	return new Proxy(class, constructHandler(traps, trapCheck));
-    };
+      set: (target, prop, value, receiver) => {
+
+	let trapFunc = settrap(prop);
+	if (trapFunc != null) {
+	  trapFunc(receiver, target, prop, value);
+	  return;
+	}
+	target[prop] = value;
+      }
+    }
+  };
+
+  const constructHandler = (gettrap, settrap) => {
+    return {
+      construct: (target, args) => {
+	let instance = new (target.bind.apply(target, args));
+	return new Proxy(instance, instanceHandler(gettrap, settrap));
+      }
+    }
+  };
+
+  proxy = (clazz, gettrap, settrap) => {
+    return new Proxy(clazz, constructHandler(gettrap, settrap));
+  };
 })();
