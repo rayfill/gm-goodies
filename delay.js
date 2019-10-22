ec74a70993b0c82480f85dd025fad9571fedd1c0
rayfill <rayfill@gmail.com>
2019-10-23 08:41:20 +0900

    add delay and xhr-hook

---
+++ delay.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function delay(func, delayTime) {
+  
+  let time = delayTime === undefined ? 0 : delayTime;
+  let id = setTimeout(func, delayTime);
+  return () => { clearTimeout(id); };
+}
---
+++ xhr-hook.js
@@ -0,0 +1,48 @@
+function xhrHook(hook) {
+  const xmlhttpRequest = unsafeWindow.XMLHttpRequest;
+
+  let methodHook = {
+    get: (xhr, prop, proxy) => {
+      let funcOrValue = xhr[prop];
+
+      if (typeof funcOrValue === "function") {
+        if (prop === "send") {
+
+          return (...args) => {
+            let savedOnload = xhr.onload;
+	    
+            xhr.onload = (...args) => {
+              hook.apply(hook, [xhr].concat(args));
+              xhr.onload = savedOnload;
+
+              if (typeof savedOnload === "function") {
+                return savedOnload.apply(xhr, args);
+              }
+            };
+	    
+            xhr.send.apply(xhr, args);
+          }
+        }
+	
+        return (...args) => {
+          let result = xhr[prop].apply(xhr, args);
+          return result;
+        };
+      }
+      return funcOrValue;
+    },
+
+    set: (xhr, prop, value, proxy) => {
+      xhr[prop] = value;
+      return true;
+    }
+  };
+  
+  let constructorHook = {
+    construct: () => {
+      return new Proxy(new xmlhttpRequest(), methodHook);
+    }
+  };
+  
+  unsafeWindow.XMLHttpRequest = new Proxy(xmlhttpRequest, constructorHook);
+}
