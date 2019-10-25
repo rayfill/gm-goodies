80feb45ca3e0e28e06b42d6b1a0c4ec543f23cda
rayfill <rayfill@gmail.com>
2019-10-26 04:58:55 +0900

    add DOM loaded handler

--- delay.js
+++ delay.js
@@ -1,10 +1,28 @@
 'use strict';
 
 function delay(func, delayTime) {
-  
+
   let time = delayTime === undefined ? 0 : delayTime;
   let id = setTimeout(func, delayTime);
   return () => { clearTimeout(id); };
 }
 
+function domContentLoaded(func) {
+  let p = new Promise((resolve) => {
+    console.log("DCL", document.readyState);
+
+    if (document.readyState !== "loading") {
+      return resolve();
+    }
+    document.addEventListener('DOMContentLoaded', () => {
+      resolve();
+    });
+  });
+  if (typeof func === "function") {
+    return p.then(func);
+  }
+  return p;
+}
+
 self.delay = delay;
+self.domContentLoaded = domContentLoaded;
