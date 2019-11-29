4a2f724ae844ead80568bc99f4e45fe1f7bb2e82
alfeim <rayfill@gmail.com>
2019-11-30 07:52:44 +0900

    remove log

--- xhr-hook.js
+++ xhr-hook.js
@@ -1,3 +1,36 @@
+function xhrHook2(hook) {
+  window.addEventListener('message', (ev) => {
+    let { type: type, responseType: resType, url: url, content: content } = ev.data;
+    if (type === "trap") {
+      hook(url, resType, content);
+    }
+  });
+
+  function trap() {
+    const origSend = window.XMLHttpRequest.prototype.send;
+    window.XMLHttpRequest.prototype.send = function() {
+      const xhr = this;
+      const args = Array.from(arguments);
+      const callback = () => {
+        window.postMessage({
+          type: "trap",
+	  responseType: xhr.responseType,
+          url: xhr.responseURL,
+          content: xhr.response
+        });
+        xhr.removeEventListener("load", callback);
+      };
+      xhr.addEventListener("load", callback);
+
+      return origSend.apply(xhr, args);
+    };
+  }
+
+  let scr = document.createElement("script");
+  scr.text = "(" + trap.toString() + ")();";
+  unsafeWindow.document.documentElement.appendChild(scr);
+}
+
 function xhrHook(hook) {
   const xmlhttpRequest = unsafeWindow.XMLHttpRequest;
 
@@ -37,13 +70,14 @@ function xhrHook(hook) {
       return true;
     }
   };
-  
+
   let constructorHook = {
     construct: () => {
       return new Proxy(new xmlhttpRequest(), methodHook);
     }
   };
-  
+
   unsafeWindow.XMLHttpRequest = new Proxy(xmlhttpRequest, constructorHook);
 }
 self.xhrHook = xhrHook;
+self.xhrHook2 = xhrHook2;
